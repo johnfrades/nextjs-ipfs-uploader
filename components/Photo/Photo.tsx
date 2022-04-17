6014e9703349266fbb9f306c06add3173ffd1db7
John Frades <johnfrades@gmail.com>
2022-04-17 15:49:03 +0800

    initial commit

---
+++ components/Photo/Photo.tsx
@@ -0,0 +1,50 @@
+import { Box, Center, chakra, Spinner, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import { useQuery } from 'react-query';
+
+type Photo = {
+  metadata: string;
+  hash: string;
+};
+
+const Photo: React.FC<Photo> = ({ metadata, hash }) => {
+  const { data, isLoading } = useQuery(hash, () =>
+    fetch(`https://ipfs.infura.io/ipfs/${metadata}`).then((res) => res.json())
+  );
+
+  return (
+    <Box minH="250px" width="350px" border="1px solid #e5e5e5" borderRadius="10px">
+      {isLoading && (
+        <Center height="100%">
+          <Spinner />
+        </Center>
+      )}
+      {!isLoading && data && (
+        <>
+          <Image width="200px" height="200px" src={data?.imageSrc} objectFit="cover" layout="responsive" />
+
+          <Box px={3} pb={5}>
+            <Center mt={2}>
+              <Text fontWeight="500" fontSize="xl">
+                {data.name}
+              </Text>
+            </Center>
+            <Box mt={5}>
+              <Text isTruncated>
+                <chakra.span fontWeight="500">File Name</chakra.span>: {data.fileName}
+              </Text>
+              <Text isTruncated>
+                <chakra.span fontWeight="500">Hash</chakra.span>: {data.fileName}
+              </Text>
+              <Text>
+                <chakra.span fontWeight="500">Description</chakra.span>: {data?.description}
+              </Text>
+            </Box>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default Photo;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+const queryClient = new QueryClient();
+
+const CustomQueryClientProvider: any = QueryClientProvider;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <CustomQueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </CustomQueryClientProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
